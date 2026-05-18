f5d279b3ca5c52046006fa3ed7ad8149233a5add
mkbastellkram <mk.madeira2026@outlook.com>
2026-05-18 11:32:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pr-explorer-v7.8';
+const CACHE='pr-explorer-v8.0';
 const ASSETS=['./','./index.html?v=7.8','./style.css?v=7.8','./app.js?v=7.8','./manifest.webmanifest?v=7.8','./icon-180.png?v=7.8','./icon-192.png?v=7.8','./icon-512.png?v=7.8'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
