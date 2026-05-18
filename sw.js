5fc21779cdd40b51a55dea5b73bb52cb14be1da1
mkbastellkram <mk.madeira2026@outlook.com>
2026-05-18 08:49:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pr-explorer-v7.5';
-const ASSETS=['./','./index.html?v=7.5','./style.css?v=7.5','./app.js?v=7.5','./manifest.webmanifest?v=7.5','./icon-180.png?v=7.5','./icon-192.png?v=7.5','./icon-512.png?v=7.5'];
+const CACHE='pr-explorer-v7.6';
+const ASSETS=['./','./index.html?v=7.6','./style.css?v=7.6','./app.js?v=7.6','./manifest.webmanifest?v=7.6','./icon-180.png?v=7.6','./icon-192.png?v=7.6','./icon-512.png?v=7.6'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{const r=e.request;if(r.mode==='navigate'){e.respondWith(fetch(r).catch(()=>caches.match('./index.html?v=7.5')));return;} e.respondWith(fetch(r).then(res=>{const copy=res.clone(); if(r.url.startsWith(self.location.origin)) caches.open(CACHE).then(c=>c.put(r,copy)); return res;}).catch(()=>caches.match(r)));});
+self.addEventListener('fetch',e=>{const r=e.request;if(r.mode==='navigate'){e.respondWith(fetch(r).catch(()=>caches.match('./index.html?v=7.6')));return;} e.respondWith(fetch(r).then(res=>{const copy=res.clone(); if(r.url.startsWith(self.location.origin)) caches.open(CACHE).then(c=>c.put(r,copy)); return res;}).catch(()=>caches.match(r)));});
