f2b71d7b2427b1e2da655c6d81b21a6914ddf88a
mkbastellkram <mk.madeira2026@outlook.com>
2026-05-20 19:51:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-const CACHE='madeira-pr-explorer-v10.5';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE='madeira-pr-v11';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
